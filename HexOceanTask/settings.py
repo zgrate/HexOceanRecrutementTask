0021740d2e695c5beb37451210990551ddcb5cab
Z-Grate <grzesiek.dzingi@gmail.com>
2023-02-18 15:00:13 +0100

    Settings modified Removed some files

--- HexOceanTask/settings.py
+++ HexOceanTask/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['*'] #This should be set somewhere else with correct auth
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
